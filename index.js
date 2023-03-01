b99fda3029225349a773dfe6a1e2879dc15b6d77
dariram <104618523+dariram@users.noreply.github.com>
2023-03-01 10:26:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import react from 'react';
-import  ReactDOM from 'react-dom/client';
 
-import App from './App';
-import './index.css';
-const root=ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<App/>);
+export { default as ChannelCard } from './ChannelCard';
+export { default as VideoCard } from './VideoCard';
+export { default as ChannelDetail } from './ChannelDetail';
+export { default as Feed } from './Feed';
+export { default as Loader } from './Loader';
+export { default as Navbar } from './Navbar';
+export { default as SearchBar } from './SearchBar';
+export { default as VideoDetail } from './VideoDetail';
+export { default as SearchFeed } from './SearchFeed';
+export { default as Videos } from './Videos';
+export { default as Sidebar } from './Sidebar';
